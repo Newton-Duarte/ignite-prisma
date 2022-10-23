6e8f1c874c4ba6bf3ea568132903ea17247263a9
Newton Duarte <newton_@hotmail.com>
2022-10-23 12:08:51 -0300

    feat: add create tags migration

---
+++ prisma/migrations/20221023150700_create_tags/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "tags" (
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "tags_pkey" PRIMARY KEY ("name")
+);
